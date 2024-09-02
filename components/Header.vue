f576dacce1c982dc424bc66d3de4090311813c2c
sxoorp <chehamikamalennour@gmail.com>
2024-09-02 10:28:14 +0100

    update: color mode switcher

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <UContainer as="div" class="flex justify-between items-center p-4">
         <ULink to="/" class="text-4xl font-extrabold transition-colors hover:text-primary">Sumire</ULink>
+        <ColorMode />
     </UContainer>
 </template>
